096c959b7cefb89a3ed6252542ccad19ee0a977e
seyiadel <seyiadel03@gmail.com>
2023-05-04 11:15:08 +0100

    Model Objects

--- event_app/models.py
+++ event_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    artwork = models.ImageField()
+    venue = models.TextField()
+    time = models.TimeField()
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = 'created_at'
--- event_app/urls.py
+++ event_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
 
-urlpattern = [
-    path(),
-]
+urlpatterns = []
+
