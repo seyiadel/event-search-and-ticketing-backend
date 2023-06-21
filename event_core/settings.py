5593cac8d0fefaa08cfa6febfc390bbd3e96cdf6
seyiadel <seyiadel03@gmail.com>
2023-06-21 02:17:28 +0100

    staticfiles of packages added i.e drf_yasg

--- event_core/settings.py
+++ event_core/settings.py
@@ -225,6 +225,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
