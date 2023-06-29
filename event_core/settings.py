a1b12bfc1ccea310351fd6a608863149e5d4c200
seyiadel <seyiadel03@gmail.com>
2023-06-29 12:16:02 +0100

    development database

--- event_core/settings.py
+++ event_core/settings.py
@@ -114,12 +114,8 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('PGNAME'),
-        "USER": os.getenv('PGUSER'),
-        "PASSWORD": os.getenv('PGPASSWORD'),
-        "HOST": os.getenv('PGHOST'),
-        "PORT": os.getenv('PGPORT'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
