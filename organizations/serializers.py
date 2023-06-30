06069796bed446513dd5af440b5478665602a95a
seyiadel <seyiadel03@gmail.com>
2023-06-30 06:47:37 +0100

    autheticate list of banks view, creator serializer primary key related

--- organizations/serializers.py
+++ organizations/serializers.py
@@ -3,11 +3,11 @@ from organizations.models import Organization
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
+    creator = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Organization
         fields = "__all__"
 
     def create(self, validated_data):
-        # validated_data['creator'] = self.context('request').user
-        return Organization.objects.create(**validated_data)
+       return Organization.objects.create(**validated_data)
 
