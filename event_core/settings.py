3ffbffb634f2998147d42f5f392cde16d714eb3c
seyiadel <seyiadel03@gmail.com>
2023-07-03 02:46:08 +0100

    database added

--- event_core/settings.py
+++ event_core/settings.py
@@ -116,18 +116,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": os.getenv('PGNAME'),
-    #     "USER": os.getenv('PGUSER'),
-    #     "PASSWORD": os.getenv('PGPASSWORD'),
-    #     "HOST": os.getenv('PGHOST'),
-    #     "PORT": os.getenv('PGPORT'),
-    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3"
-        }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('PGNAME'),
+        "USER": os.getenv('PGUSER'),
+        "PASSWORD": os.getenv('PGPASSWORD'),
+        "HOST": os.getenv('PGHOST'),
+        "PORT": os.getenv('PGPORT'),
+    }
 }
 
 
