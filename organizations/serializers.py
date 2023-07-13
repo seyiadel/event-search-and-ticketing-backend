ed50d7f7fd25d5193be775a610ec44fbbcdf97dd
seyiadel <seyiadel03@gmail.com>
2023-07-13 01:23:56 +0100

    revert get organization serializer

--- organizations/serializers.py
+++ organizations/serializers.py
@@ -6,14 +6,7 @@ class OrganizationSerializer(serializers.ModelSerializer):
     creator = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Organization
-        fields = ['name','bio','creator', 'created_at']
+        fields = ['id', 'name','bio','creator', 'created_at']
 
     def create(self, validated_data):
        return Organization.objects.create(**validated_data)
-
-
-class GetOrganizationSerializer(serializers.ModelSerializer):
-    creator = serializers.PrimaryKeyRelatedField(read_only=True)
-    class Meta:
-        model = Organization
-        fields = ['id','name','bio','creator', 'created_at']
--- organizations/views.py
+++ organizations/views.py
@@ -1,5 +1,5 @@
 from rest_framework import views, response, permissions
-from organizations.serializers import OrganizationSerializer, GetOrganizationSerializer
+from organizations.serializers import OrganizationSerializer
 from organizations.models import Organization
 from drf_yasg.utils import swagger_auto_schema
 from knox.auth import TokenAuthentication as KnoxTokenAuthentication
@@ -22,7 +22,7 @@ class OrganizationView(views.APIView):
             return response.Response(data=serializer.data, status=201)
         return response.Response(data=serializer.errors, status=400)
 
-    @swagger_auto_schema(responses=GetOrganizationSerializer)
+    @swagger_auto_schema(OrganizationSerializer)
     def get(self, request):
         "Get all Organizations by Logged In User"
         organization = Organization.objects.filter(creator=request.user)
@@ -34,7 +34,7 @@ class SingleOrganizationView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [KnoxTokenAuthentication,]
 
-    @swagger_auto_schema(responses=GetOrganizationSerializer)
+    @swagger_auto_schema(OrganizationSerializer)
     def get(self, request, organization_id):
         "Get single organization by Logged In User and organization_id"
         organization = Organization.objects.filter(creator=request.user).get(id=organization_id)
